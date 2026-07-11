7d2f676cb6471844c89f25a59f76e91ae06d7ce5
kuzuanpa <misaka16384@outlook.com>
2026-07-11 12:16:22 +0800

    license

--- src/main/java/cn/kuzuanpa/organapi/common/inventory/OrganPartContainer.java
+++ src/main/java/cn/kuzuanpa/organapi/common/inventory/OrganPartContainer.java
@@ -1,3 +1,18 @@
+/*
+ * This class was created by <kuzuanpa>. It is distributed as
+ * part of the organAPI Mod. Get the Source Code in github:
+ * https://github.com/KuzuMuku/organAPI
+ *
+ * organAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+
+ * organAPI is Open Source and distributed under the
+ * AGPLv3 License: https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ */
+
 package cn.kuzuanpa.organapi.common.inventory;
 
 import cn.kuzuanpa.organapi.common.capability.IOrganHolder;
